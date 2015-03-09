5729c17354b0fa233f739e91480408281c69bc4c
Iris DE-VILLARS <ide-vill@e1r10p14.42.fr>
2015-03-09 15:55:24 +0100

    strcat

--- src/ft_strcat.s
+++ src/ft_strcat.s
@@ -3,18 +3,21 @@ section .text
 global	_ft_strcat
 
 _ft_strcat:
-	push	rdi
+		push	rdi
+
+_loop1:
+		cmp		rdi, '0'
+		je		_loop2
+		inc		rdi
+		jmp		_loop1
 
 
-	mov 	rcx, rsi
-	mov 	rax, 0
+_loop2:
+		cmp		rsi, '0'
+		je		_ret
+		movsb
+		jmp		_loop2
 
-	rep		movsb
-
-	pop		rax
-
-	mov		rdi, 1
-	push	10
-	pop		rax
-	
-	ret
+_ret:
+		pop		rax
+		ret
